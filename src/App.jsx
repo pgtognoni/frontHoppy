ec0da8d4b627a1ca82850b561c47d495b9f502d0
Charlie Trestini <charlietrestini@hotmail.com>
2023-03-01 18:55:52 +0000

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import Sidebar from './components/Sidebar'
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import { Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
-import ProfilePage from './pages/ProfilePage'
-import PrivateRoute from './components/PrivateRoute'
+import Sidebar from "./components/Sidebar";
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProfilePage from "./pages/ProfilePage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-
   return (
     <div className="App">
       <Navbar />
-      <Sidebar />
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/profile" element={
-          <PrivateRoute>
-            <ProfilePage />
-          </PrivateRoute>
-          } />
-      </Routes>
+      <div className="bodyContainer">
+        <Sidebar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <ProfilePage />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
