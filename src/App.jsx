845d0d74e8072051775726d8f485c0389de5896d
Abiel Isak <abizmo99@gmail.com>
2023-03-03 11:28:40 +0100

    Post form skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import SignupPage from "./pages/SignupPage";
 import ProfilePage from "./pages/ProfilePage";
 import PrivateRoute from "./components/PrivateRoute";
 import LandingPage from "./pages/LandingPage";
+import NewPost from "./pages/NewPost";
+import PostDetils from "./pages/PostDetils";
+import UpdatePost from "./pages/UpdatePost";
 
 
 
@@ -20,6 +23,9 @@ function App() {
         <div className='body-content landingPage'>
           <Routes>
             <Route path="/" element={<LandingPage />} />
+            <Route path="/:postId" element={<PostDetils />} />
+            <Route path="/new" element={<NewPost/>}/>
+            <Route path="/update/:postId" element={<UpdatePost/>} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/profile" element={
