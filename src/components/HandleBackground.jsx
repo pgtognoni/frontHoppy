1058aee83bc028a8fadb73d927f6fb9ab6b75fe1
Charlie Trestini <charlietrestini@hotmail.com>
2023-03-09 14:18:18 +0000

    background

--- src/components/HandleBackground.jsx
+++ src/components/HandleBackground.jsx
@@ -9,20 +9,20 @@ function HandleBackground() {
 
 
     const backgroundImagesShow1 = [
-        "./image/Untitled - Copy@1-1904x993.png",
+        "./image/Untitled-Copy@1-1904x993.png",
         "./image/desktop-wallpaper-sky-blue-clouds-digital-art-chromebook-pixel-background-and-cloud-pixel-art.jpg",
         "./image/164775-water-liquid-fluid-painting-art-1920x1080.jpg",
       ];
     
       const handleBackground = (image) => {
-        if (image === "./image/Untitled - Copy@1-1904x993.png") {
+        if (image === "./image/Untitled-Copy@1-1904x993.png") {
           setBackgroundImages([
             "imageBackgrounChange1",
             "imageBackgrounChange2",
             "imageBackgrounChange3",
           ]);
           setBackgroundImagesApply([
-            "./image/Untitled - Copy@1-1904x993.png",
+            "./image/Untitled-Copy@1-1904x993.png",
             "./image/desktop-wallpaper-sky-blue-clouds-digital-art-chromebook-pixel-background-and-cloud-pixel-art.jpg",
             "./image/164775-water-liquid-fluid-painting-art-1920x1080.jpg",
           ]);
@@ -40,7 +40,7 @@ function HandleBackground() {
           setBackgroundImagesApply([        
             "./image/desktop-wallpaper-sky-blue-clouds-digital-art-chromebook-pixel-background-and-cloud-pixel-art.jpg",
             "./image/164775-water-liquid-fluid-painting-art-1920x1080.jpg",
-            "./image/Untitled - Copy@1-1904x993.png",
+            "./image/Untitled-Copy@1-1904x993.png",
           ]);
           setBackground(backgroundImagesShow1[1]);
         }
@@ -54,7 +54,7 @@ function HandleBackground() {
           ]);
           setBackgroundImagesApply([
             "./image/164775-water-liquid-fluid-painting-art-1920x1080.jpg",
-            "./image/Untitled - Copy@1-1904x993.png",
+            "./image/Untitled-Copy@1-1904x993.png",
             "./image/desktop-wallpaper-sky-blue-clouds-digital-art-chromebook-pixel-background-and-cloud-pixel-art.jpg",        
           ]);
           setBackground(backgroundImagesShow1[2]);
