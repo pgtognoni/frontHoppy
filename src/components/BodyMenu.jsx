ba247a9c45adc11eb65d533159a4815bb1c69a52
pgtognoni <pgtognoni@gmail.com>
2023-03-09 18:31:00 +0000

    side bar

--- src/components/BodyMenu.jsx
+++ src/components/BodyMenu.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HandleBackground from './HandleBackground'
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useLocation } from 'react-router-dom';
 import PostForm from './posts/PostForm';
+import MobileNavbar from './MobileNavbar';
 
 function BodyMenu(props) {
-
+    const [ responseMessage, setResponseMessage ] = useState("");
 const { openModal, setPostsCall, posts, setPosts, setAddNewPost } = props
 const location = useLocation().pathname;
 
@@ -19,12 +20,13 @@ const location = useLocation().pathname;
         </div>
         {location === '/' && 
         <div className='btn-menu-container add-new-btn'>
-        <button type="button" class="btn btn-primary dropdown-item btn-reset-style" data-bs-toggle="modal" data-bs-target="#createNewPost" >
+        <button type="button" class="add-new-post add-new-btn" data-bs-toggle="modal" data-bs-target="#createNewPost" >
                 <span className="btn-add">➕</span>
                 <p className='text-hide'>Add New Post</p>
             </button>
         </div>
         }
+        <MobileNavbar post={posts} setPosts={setPosts} responseMessage={responseMessage} setResponseMessage={setResponseMessage} />
         <PostForm 
               setPostsCall={setPostsCall}
               posts={posts}
--- src/pages/GroupsPage.jsx
+++ src/pages/GroupsPage.jsx
@@ -10,6 +10,9 @@ const Store = () => {
   const { background,  backgroundImages, setBackgroundImages }=useContext(SessionContext);
   const [loading, isLoading] = useState(true);
   const [groups, setGroups] = useState();
+  const [ tag, setTag ] = useState();
+  const [ section, setSection] = useState();
+  const [type, setType] = useState();
 
   const fetchData = async () => {
     const response = await axios.get(`${BACK_URL}/groups/`);
@@ -28,12 +31,46 @@ const Store = () => {
       }
   }, [groups])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="column-center">
       <BodyMenu />
       {!loading ? (
         <>
         <h1 className="groups-page-title postTitle">The Hoppy Community</h1>
+        <form className='storeBody group-search storeBody' onSubmit={(e) => handleSubmit(e)}>
+          <div className="mb-3">
+            <button className="logout btn-sidebar dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                <p className=''>{type ? type : "type"}</p>
+            </button>
+            <ul className="dropdown-menu">
+                <li><p onClick={(event) => setType("tag")} className="dropdown-item" value="tag" >Hashtag</p></li>
+                <li><p onClick={(event) => setType("category")} className="dropdown-item" value="category" >Category</p></li>
+            </ul>
+          </div>
+          {type === 'category' 
+            ? <div className="mb-3">
+              <button className="logout btn-sidebar dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                  <p className=''>{section ? section : "section"}</p>
+              </button>
+              <ul className="dropdown-menu">
+                  <li><p onClick={(event) => setSection("meme")} className="dropdown-item" value="meme">Memes</p></li>
+                  <li><p onClick={(event) => setSection("lifestyle")} className="dropdown-item" value="lifestyle">Lifestyle</p></li>
+                  <li><p onClick={(event) => setSection("educational")} className="dropdown-item" value="educational">Education</p></li>
+                  <li><p onClick={(event) => setSection("gaming")} className="dropdown-item" value="gaming">Gaming</p></li>
+                  <li><p onClick={(event) => setSection("food")} className="dropdown-item" value="food">Food</p></li>
+                  <li><p onClick={(event) => setSection("business")} className="dropdown-item" value="business">Business</p></li>
+              </ul>
+            </div>
+            : <div className="mb-3">
+                  <label htmlFor="tag" className="col-form-label"></label>
+                  <input type="text" className="form-control" id="tag" value={tag} onChange={(e) => {setTag(e.target.value)}} />
+            </div>
+          }
+        </form>
         {groups.map((group) => {
           return (
             <GroupCard group={group} key={group._id} />
