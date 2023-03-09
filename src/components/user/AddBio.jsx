16baf054dc10e61427acf69b30d41195fa5406f5
pgtognoni <pgtognoni@gmail.com>
2023-03-09 17:47:01 +0000

    latest so far

--- src/components/user/AddBio.jsx
+++ src/components/user/AddBio.jsx
@@ -33,7 +33,6 @@ function AddBio(props) {
     <div className="modal-dialog">
     <div className="modal-content">
         <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">New Group</h5>
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div className="modal-body">
