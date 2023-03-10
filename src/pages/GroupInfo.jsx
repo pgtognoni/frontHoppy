e9d7233bbf6cf99aa687f8a0df7f0a4c0833eba9
Charlie Trestini <charlietrestini@hotmail.com>
2023-03-10 09:58:19 +0000

    what

--- src/pages/GroupInfo.jsx
+++ src/pages/GroupInfo.jsx
@@ -17,7 +17,7 @@ const BACK_URL = import.meta.env.VITE_BACK_URL;
 function GroupInfo () {
     const { background,  backgroundImages, setBackgroundImages, setUser, user }=useContext(SessionContext);
     const { groupPostsCtx, setGroupPostsCtx } = useContext(PostContext);
-
+    
     const [group, setGroup] = useState();
     const [ groupPosts, setGroupPosts ] = useState([]);
     const [ members, setMembers ] = useState([]);
@@ -291,7 +291,8 @@ function GroupInfo () {
           </div>
         )}
       </div>
-      <img className="background3d" src={background}></img>
+      {console.log("BACKGROUND", background)}
+      <img className="background3d" src={`../public/${background}`}></img>
     </>
   );
 }
